6203102ceed748c034685f60b1a3a32d256fed40
Monakyaa <dewimn.rizki@gmail.com>
2025-05-23 09:43:18 +0700

    Initial commit

---
+++ frontend-uas-230102058/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+use App\Http\Controllers\DataMahasiswaController;
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+
+Route::get('/', [DataMahasiswaController::class, 'index']); // Halaman utama
+Route::get('/mahasiswa', [DataMahasiswaController::class, 'index']); // Ini penting!
+Route::get('/mahasiswa/create', [DataMahasiswaController::class, 'create']);
+Route::post('/mahasiswa', [DataMahasiswaController::class, 'store']);
+Route::get('/mahasiswa/{id}/edit', [DataMahasiswaController::class, 'edit']);
+Route::put('/mahasiswa/{id}', [DataMahasiswaController::class, 'update']);
+Route::delete('/mahasiswa/{id}', [DataMahasiswaController::class, 'destroy']);
